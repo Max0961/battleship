ba29f8a2a7645f9182bcf4a568536391e63d1d3a
Maxam <samsung.096164@gmail.com>
2022-12-23 16:42:34 +0300

    updated

---
+++ battleship.py
@@ -0,0 +1,495 @@
+import time as t
+from random import randint
+import ctypes
+kernel32 = ctypes.windll.kernel32
+kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
+
+
+class Settings:
+    size = 10
+
+    ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
+
+    appearance = {
+        'none': ' ┤',
+        'ship-space': ' ┤',
+        'ship': '▓▓',
+        'miss': '  ',
+        'hit': '}{'
+    }
+
+    @property
+    def ship_test(self):
+        return [Ship(4, 1, 3, 5), Ship(2, 1, 0, 7), Ship(2, 0, 4, 0),
+                Ship(1, 0, 6, 4), Ship(1, 0, 6, 6), Ship(1, 0, 1, 1),
+                Ship(1, 0, 3, 3)]
+
+
+class BoardException(Exception):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class BoardUsedException(BoardException):
+    def __str__(self):
+        return 'В эту клетку уже стреляли'
+
+
+class BoardOutException(BoardException):
+    def __str__(self):
+        return f'Клетка {int_to_char(self.x)}{add_one(self.y)} вне доски'
+
+
+class ShipGenerationFail(Exception):
+    def __str__(self):
+        return f'не удалось разместить корабли'
+
+
+class TooLargeBoardException(Exception):
+    def __str__(self):
+        return f'Размер доски должен быть не больше 26'
+
+
+class TooLargeShipException(Exception):
+    def __str__(self):
+        return f'Длина коробля должена быть не больше 5'
+
+
+class WrongNumberException(Exception):
+    def __str__(self):
+        return f'Введите положительное число'
+
+
+class WrongCharException(Exception):
+    def __str__(self):
+        return f'Введите строчную букву латиницы'
+
+
+def int_to_char(number):
+    if 0 <= number <= 25:
+        return chr(number + 97)
+    else:
+        raise WrongNumberException()
+
+
+def char_to_int(char):
+    if 'a' <= char <= 'z':
+        return ord(char) - 97
+    else:
+        raise WrongCharException()
+
+
+def add_one(number):
+    return number + 1
+
+
+def subtract_one(number):
+    return number - 1
+
+
+class Board:
+    def __init__(self, size, is_enemy):
+        if size > 26:
+            raise TooLargeBoardException()
+        self.size = size
+        self.is_enemy = is_enemy
+        self.ships = []
+        self.board = [Cell(self, x, y)
+                      for x in range(size)
+                      for y in range(size)]
+
+    @property
+    def is_defeated(self):
+        return all(map(lambda x: x.is_destroyed, self.ships))
+
+    def get_cell(self, x, y):
+        if 0 <= x < self.size and 0 <= y < self.size:
+            return self.board[x * self.size + y]
+
+    def has_space(self, ship):
+        for d in range(ship.length):
+            if ship.orientation:
+                x, y = ship.x + d, ship.y
+            else:
+                x, y = ship.x, ship.y + d
+
+            if not self.get_cell(x, y) or self.get_cell(x, y).is_space:
+                return False
+
+        return True
+
+    def get_neighborhood(self, cell, orientation=None):
+        if orientation is None:
+            return (self.get_cell(cell.x + dx, cell.y + dy)
+                    for dx in (-1, 0, 1) for dy in (-1, 0, 1))
+
+        elif orientation == 1:
+            return (self.get_cell(cell.x, cell.y + dy)
+                    for dy in (-1, 1))
+        elif orientation == 0:
+            return (self.get_cell(cell.x + dx, cell.y)
+                    for dx in (-1, 1))
+        else:
+            return (self.get_cell(cell.x - 1, cell.y),
+                    self.get_cell(cell.x + 1, cell.y),
+                    self.get_cell(cell.x, cell.y - 1),
+                    self.get_cell(cell.x, cell.y + 1))
+
+    def build(self, ship):
+        if not self.has_space(ship):
+            return False
+
+        cells = []
+
+        for d in range(ship.length):
+            if ship.orientation:
+                cells.append(self.get_cell(ship.x + d, ship.y))
+            else:
+                cells.append(self.get_cell(ship.x, ship.y + d))
+
+        for cell in cells:
+            cell.ship = ship
+
+        for cell in cells:
+            for space_cell in self.get_neighborhood(cell):
+                if space_cell and not space_cell.ship:
+                    space_cell.is_space = True
+                    ship.space_cells.append(space_cell)
+
+        return True
+
+    def get_not_shot_random_cell(self, special=None):
+        if not special:
+            cells = filter(lambda x: not x.shot_result_key, self.board)
+        else:
+            cells = filter(lambda x: not x.shot_result_key, special)
+
+        cells = list(cells)
+
+        if cells:
+            return cells[randint(0, len(cells) - 1)]
+
+    def get_free_random_cell(self):
+        cells = filter(lambda x: not x.ship and not x.is_space, self.board)
+
+        cells = list(cells)
+
+        if cells:
+            return cells[randint(0, len(cells) - 1)]
+
+    def generate(self):
+        for ship_type in Settings.ships:
+            result = False
+            counter = 0
+
+            while not result:
+                counter += 1
+                random_cell = self.get_free_random_cell()
+
+                if not random_cell or counter > self.size * self.size:
+                    raise ShipGenerationFail()
+
+                ship = Ship(
+                    ship_type,
+                    randint(0, 1),
+                    random_cell.x,
+                    random_cell.y
+                )
+
+                result = self.build(ship)
+
+                if result:
+                    self.ships.append(ship)
+
+    def build_ships(self, ships):
+        self.ships = ships
+        for ship in ships:
+            self.build(ship)
+
+    def cell_out(self, x, y):
+        if not 0 <= x < self.size or not 0 <= y < self.size:
+            raise BoardOutException(x, y)
+
+    def __str__(self):
+        parts = [' ' * (len(str(self.size)) + 1),
+                ' '.join(map(int_to_char, range(0, self.size))), '\n',
+                ' ' * len(str(self.size)), '╔', '═' * self.size * 2, '╗', '\n']
+
+        for y in range(self.size):
+            parts.append(str(add_one(y)))
+            parts.append(' ' * (len(str(add_one(self.size))) - len(str(add_one(y)))))
+            parts.append('║')
+
+            for x in range(self.size):
+                parts.append(f'{self.get_cell(x, y)}')
+
+            parts.append('║\n')
+
+        parts += [' ' * len(str(self.size)), '╚', '═' * self.size * 2, '╝', '\n']
+        return ''.join(parts)
+
+
+class Ship:
+    def __init__(self, length, orientation, x, y):
+        if length > 5:
+            raise TooLargeShipException()
+        self.length = length
+        self.orientation = orientation
+        self.x = x
+        self.y = y
+        self.lives = length
+        self.space_cells = []
+        self.names = ['Катер', 'Эсминец', 'Крейсер', 'Линкор', 'Авианосец']
+
+    @property
+    def is_destroyed(self):
+        return not self.lives
+
+    @property
+    def name(self):
+        return self.names[self.length - 1]
+
+    def hit(self):
+        self.lives -= 1
+
+        if self.is_destroyed:
+            for cell in self.space_cells:
+                if not cell.shot_result_key:
+                    cell.fire()
+
+
+class Cell:
+    def __init__(self, board, x, y):
+        self.board = board
+        self.x = x
+        self.y = y
+        self.is_space = False
+        self.ship = None
+        self.shot_results = {'miss': 'Мимо!', 'hit': 'Есть пробитие!',
+                             'destroyed': 'Корабль уничтожен!'}
+        self.shot_result_key = None
+
+    @property
+    def shot_result_text(self):
+        if self.shot_result_key:
+            text = self.shot_results[self.shot_result_key]
+            if self.shot_result_key == 'destroyed':
+                text = text.replace('Корабль', self.ship.name)
+            return text
+
+    def fire(self):
+        if self.shot_result_key:
+            raise BoardUsedException(self.x, self.y)
+
+        if self.ship:
+            self.ship.hit()
+            self.shot_result_key = 'hit'
+
+            if self.ship.is_destroyed:
+                self.shot_result_key = 'destroyed'
+        else:
+            self.shot_result_key = 'miss'
+
+    def __str__(self):
+        if self.shot_result_key == 'hit' or self.shot_result_key == 'destroyed':
+            return Settings.appearance['hit']
+
+        if self.shot_result_key == 'miss':
+            return Settings.appearance['miss']
+
+        if self.ship and not self.shot_result_key and not self.board.is_enemy:
+            return Settings.appearance['ship']
+
+        if self.is_space and not self.board.is_enemy:
+            return Settings.appearance['ship-space']
+
+        return Settings.appearance['none']
+
+
+class Player:
+    def __init__(self, game, enemy, colorCode):
+        self.enemy = enemy
+        self.game = game
+        self.colorCode = colorCode
+        self.shot_cell = None
+
+    def ask(self):
+        print(self.shot_cell.shot_result_text)
+        print(self.colorCode.format(self.enemy))
+        print('\033[0m')
+
+        if self.won:
+            print(self.win_text)
+            self.game.ended = True
+
+    def move(self):
+        print(self.move_text)
+
+        self.ask()
+        while self.shot_cell.shot_result_key != 'miss' and not self.game.ended:
+            self.ask()
+
+    @property
+    def win_text(self):
+        raise NotImplementedError()
+
+    @property
+    def move_text(self):
+        raise NotImplementedError()
+
+    @property
+    def won(self):
+        return self.enemy.is_defeated
+
+
+class AI(Player):
+    hit_sequence = []
+
+    def get_orientation(self):
+        if len(self.hit_sequence) > 1:
+            return self.hit_sequence[-1].x == self.hit_sequence[-2].x
+        return 2
+
+    def try_to_finish_off(self):
+        candidates = [_ for _ in self.enemy.get_neighborhood(
+            self.hit_sequence[-1], self.get_orientation()) if _]
+        t.sleep(1)
+        return self.enemy.get_not_shot_random_cell(candidates)
+
+    def random_fire(self):
+        t.sleep(1.5)
+        return self.enemy.get_not_shot_random_cell()
+
+    def ask(self):
+        shot_cell = None
+
+        # если есть попадание,
+        # пытаемся найти оставшиеся клетки в окрестности
+        if self.hit_sequence:
+            shot_cell = self.try_to_finish_off()
+
+            # если свободных клеток нет, то корабль уничтожен, или нужно
+            # продолжить с другой стороны
+            if not shot_cell:
+                if self.hit_sequence[-1].shot_result_key == 'destroyed':
+                    self.hit_sequence.clear()
+                else:
+                    self.hit_sequence.reverse()
+                    shot_cell = self.try_to_finish_off()
+
+        if not shot_cell:
+            shot_cell = self.random_fire()
+
+        print(f'Ход компьютера: {int_to_char(shot_cell.x)}{add_one(shot_cell.y)}')
+        shot_cell.fire()
+
+        if shot_cell.shot_result_key != 'miss':
+            self.hit_sequence.append(shot_cell)
+
+        self.shot_cell = shot_cell
+        super().ask()
+
+    @property
+    def win_text(self):
+        return 'Выиграл компютер!'
+
+    @property
+    def move_text(self):
+        return 'Ходит компьютер!'
+
+
+class User(Player):
+    def ask(self):
+        while True:
+            input_string = input("Ваш ход: ").replace(' ', '')
+
+            x, y = input_string[0:1], input_string[1:]
+
+            if not x.isalpha():
+                print("Первая координата должна быть буквой! ")
+                continue
+
+            if not y.isdigit():
+                print("Вторая координата должна быть числом! ")
+                continue
+
+            try:
+                x, y = char_to_int(x), subtract_one(int(y))
+            except WrongCharException as e:
+                print(e)
+                continue
+
+            try:
+                self.enemy.cell_out(x, y)
+            except BoardOutException as e:
+                print(e)
+                continue
+
+            shot_cell = self.enemy.get_cell(x, y)
+
+            try:
+                shot_cell.fire()
+            except BoardUsedException as e:
+                print(e)
+                continue
+
+            self.shot_cell = shot_cell
+            super().ask()
+            break
+
+    @property
+    def win_text(self):
+        return 'Выиграл пользователь!'
+
+    @property
+    def move_text(self):
+        return 'Ходит пользователь!'
+
+
+class Game:
+    def __init__(self):
+        self.board_size = Settings.size
+
+        self.user_board = Board(self.board_size, False)
+        # self.user_board.build_ships(Settings().ship_test)
+        self.user_board.generate()
+
+        self.computer_board = Board(self.board_size, True)
+        # self.computer_board.build_ships(Settings().ship_test)
+        self.computer_board.generate()
+
+        self.user = User(self, self.computer_board, '\033[34m {}')
+        self.computer = AI(self, self.user_board, '\033[31m {}')
+
+        self.ended = False
+
+    @staticmethod
+    def greet():
+        print("Дороу ")
+        print("формат ввода: <столбец><строка>")
+        print()
+
+    def loop(self):
+        print("Доска пользователя:")
+        print('\033[0m')
+        print('\033[37m {}'.format(self.user_board))
+        print('\033[0m')
+        print()
+
+        counter = 0
+        while not self.ended:
+            if not counter % 2:
+                self.user.move()
+            else:
+                self.computer.move()
+
+            counter += 1
+
+    def start(self):
+        self.greet()
+        self.loop()
+
+
+g = Game()
+g.start()
